9791ec845cd0cf3b579001b8bafa4f695d190ab9
unknown <oet112@naver.com>
2022-03-29 16:40:40 +0900

    routes and blog

---
+++ src/models/Blog.js
@@ -0,0 +1,17 @@
+const { Schema, model, Types } = require("mongoose");
+
+const BlogSchema = new Schema(
+  {
+    title: { type: String, require: true },
+    content: { type: String, require: true },
+    islive: { type: Boolean, require: true, default: false },
+    user: { type: Types.ObjectId, required: true, ref: "user" },
+  },
+  { timestamps: true }
+);
+
+const Blog = model("blog", BlogSchema);
+
+module.exports = {
+  Blog,
+};
--- src/server.js
+++ src/server.js
@@ -1,52 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const { User } = require('./models/User');
+const { userRouter } = require("./routes/userRoute");
+const { blogRouter } = require("./routes/blogRoute");
+const mongoose = require("mongoose");
 
-const MONGO_URI = 'mongodb+srv://admin:mARcL5KcoDDLFgSX@mongodbtutorial.1zhzs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const MONGO_URI =
+  "mongodb+srv://admin:mARcL5KcoDDLFgSX@mongodbtutorial.1zhzs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-const server = async() => {
-    try{
-        await mongoose.connect(MONGO_URI);
-        console.log('mongodb conected');
+const server = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    mongoose.set("debug", true);
+    console.log("mongodb conected");
 
-        app.use(express.json());
+    app.use(express.json());
 
-        app.get('/user', async (req, res) => {
-            try{
-                const users = await User.find({});
-                return res.send({ users });
-            }catch(err){
-                console.log(err);
-                return res.status(500).send({ err: err.message });
-            }
-            
-        })
-
-        app.post('/user', async (req, res) => {
-            try{
-                let { username, name } = req.body;
-                if(!username) return res.status(400).send({ err: 'username required'});
-                if(!name || !name.first || !name.last) return res.status(400).send({ err: 'first and last name required'})
-                const user = new User(req.body);
-                await user.save();
-                return res.send({ user });
-            }catch(err){
-                console.log(err);
-                return res.status(500).send({ err: err.message });
-            }
-            
-        })
-
-        app.listen(3000, function(){
-            console.log('server listening on port 3000');
-        })
-    }catch(err){
-        console.log(err);
-    }
-    
-}
+    app.use("/user", userRouter);
+    app.use("/blog", blogRouter);
+    app.listen(3000, function () {
+      console.log("server listening on port 3000");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 server();
-
-
